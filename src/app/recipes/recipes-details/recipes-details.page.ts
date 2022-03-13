9750be886b044768e96fb3597829cf9bb7a1cbad
Jood80 <shawar.nujood@gmail.com>
2022-03-13 17:11:36 +0200

    chore(navigate): activate param route for details page

--- src/app/recipes/recipes-details/recipes-details.page.ts
+++ src/app/recipes/recipes-details/recipes-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipes } from '../recipes.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipes-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-details.page.scss'],
 })
 export class RecipesDetailsPage implements OnInit {
+  loadedRecipe: Recipes;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('recipeId')) {
+        return; // for now instead of redirect
+      }
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
   }
-
 }
